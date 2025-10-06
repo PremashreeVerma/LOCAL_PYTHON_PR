8c8329e9f4b34eec4108acba5ffa3349df9e6523
Premashree Verma <premaverma2000@gmail.com>
2025-10-06 15:47:05 +0530

    new dict _question file for dictionary practice

---
+++ A_dict/dict_question.py
@@ -0,0 +1,7 @@
+#In this example, we will sort the dictionary by keys and the result type will be a dictionary. 
+
+d = {'ravi': 10, 'rajnish': 9, 'sanjeev': 15}
+mkey= list(d.keys())
+mvalue= list(d.values())
+print(mkey)
+print(mvalue)
