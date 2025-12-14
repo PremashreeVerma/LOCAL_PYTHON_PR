9d33f6e70360f2d877d47ac2a9a237527998258c
Premashree Verma <premaverma2000@gmail.com>
2025-12-14 14:02:59 +0530

    your message

--- Pandas_Prac/Pandas_try_1.py
+++ Pandas_Prac/Pandas_try_1.py
@@ -2,7 +2,18 @@ import pandas as pd
 s=pd.Series([1,2,3,4,5])
 print(s)
 print(s.dtype)
+print()
+print()
+s.name="series"
+print(s.name)
 f=pd.Series(['a','b','c','d','e'])
 print(f)
+print(f[0:3])
+print(f.iloc[3])
+print(f.iloc[[3,4,1]])
 print(f.dtype)
+index=['1st','2nd','3rd','4th','5th']
+s.index=index
 print("pandas is successfully imported")
+print(s)
+
---
+++ qr_code/qr_1.py
@@ -0,0 +1,4 @@
+import qrcode as qr
+img=qr.make("https://www.youtube.com/watch?v=QcSkNb0SFdQ&list=RDMMQcSkNb0SFdQ&start_radio=1")
+img.save("nadhive_song.png")
+
